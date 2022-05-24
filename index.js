a763542a4ca73e8ea17f4201029ca58cc081c552
Annabelle <63426032+annabellechung@users.noreply.github.com>
2022-05-24 12:39:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+
+const TOKEN = "OTc4NjcyMDEyMTE5NjUwMzM0.GYSOXl.nNvrQzw5R7cftv_EI05B2CLMhLxPmCDE32LuX8" // my token was here
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "W"){
+        message.reply("Congrats my folded squad, keep being amazing")
+    } if (message.content == "L"){
+        message.reply("Get them next time guys, hang in there")
+    } if (message.content == "ace"){
+        message.reply("absolutely stunning")
+    } if (message.content == "fumbled"){
+        message.reply("ah yes, fumbling is quite rough, but don't give up, never give up")
+    } if (message.content == "TJ Cep Brandon Aybihnayve and Poop"){
+        message.reply("folded squad all together... brilliant")
+    }
+})
+
+client.login(TOKEN)
+
